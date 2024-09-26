0b8f17e95064ad8a0588ab6744cd18b7a1dc3855
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-25 21:10:31 -0400

    Update PP.py

--- PP.py
+++ PP.py
@@ -1,3 +1,24 @@
 
+def q1():
+  #Write Assignment code here
 
-#Write Assignment code here
+def q2():
+  #Write Assignment code here
+
+def q3():
+  #Write Assignment code here
+
+def q4():
+  #Write Assignment code here
+
+def q5():
+  #Write Assignment code here
+
+#Do not edit code after this
+#Comment out the followwing code when running tests
+
+q1()
+q2()
+q3()
+q4()
+q5()
